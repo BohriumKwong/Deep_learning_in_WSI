e0d67b906c651d4298b777c85a19b5f79053081e
BohriumKwong <bohrium.kwong@gmail.com>
2020-03-07 22:25:53 +0800

    同步其他项目的opencv_utils封装。

--- utils/opencv_utils.py
+++ utils/opencv_utils.py
@@ -11,11 +11,12 @@ class OpenCV:
     def __init__(self, im):
         self._im = im
 
-    def gray_binary(self, thresh=230, show=False):
+    def gray_binary(self, thresh=200, show=False,binary_arg = cv2.THRESH_BINARY_INV):
         """
         convert image to binary image after gray scale
         :param thresh: binary image threshold
         :param show: show the binary dynamically
+        :param binary_arg:cv2.THRESH_BINARY(0) or cv2.THRESH_BINARY_INV(1)
         :return:
         """
         if len(self._im.shape) >2 :
@@ -23,10 +24,10 @@ class OpenCV:
         else:
             gray = np.uint8(self._im)
         blurred = cv2.GaussianBlur(gray, (5,5), 0)
-        binary = cv2.threshold(blurred, thresh, 255, cv2.THRESH_BINARY_INV)[1]
+        binary = cv2.threshold(blurred, thresh, 255, binary_arg)[1]
 
         def binary_theshold(threshold):
-            self.binary = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY_INV)[1]
+            self.binary = cv2.threshold(blurred, threshold, 255, binary_arg)[1]
             cv2.imshow("binary image", self.binary)
 
         if show:
@@ -80,33 +81,61 @@ class OpenCV:
 
         return morphology
     
-    def find_contours(self,is_binary = False,is_erode_dilate = False,mode=3):
+#    if is_morph is not None and is_morph in ['dilate','erode','open']:
+    
+    def morphologyex_process(self,is_binary = True,kernel = np.ones((3, 3), np.uint8),method = 'dilate',iterations=3):
+        if not is_binary:
+            morphology = self.gray_binary(thresh=200, show=False)
+        else:
+            morphology = self._im
+        
+        if method is not None and method in ['dilate','erode','open','close']:
+            if method == 'dilate':
+                morphology = cv2.dilate(morphology, kernel, iterations = iterations)
+            elif method == 'erode':
+                morphology = cv2.erode(morphology, kernel, iterations = iterations)
+            elif method == 'open':
+                morphology = cv2.morphologyEx(morphology, cv2.MORPH_OPEN, kernel, iterations=iterations)
+            elif method == 'close':
+                morphology = cv2.morphologyEx(morphology, cv2.MORPH_CLOSE, kernel, iterations=iterations)        
+        return morphology               
+
+            
+    def find_contours(self,is_erode_dilate = False,is_hier_return = False,is_morphologyex = None,iterations=3,is_binary=True,mode= 3):
         """
         get the points of contours
         :param morphology: output of self.erode_dilate
         :return:
         """
-        if is_binary:
-            morphology = np.uint8(self._im)
+        if not is_binary:
+            morphology = self.gray_binary(thresh=200, show=False)
         else:
-            morphology = self.gray_binary(thresh=230, show=False)
+            morphology = self._im
+            
         if is_erode_dilate:
             morphology = self.erode_dilate(morphology, erode_iter=0, dilate_iter=3, show=False)
         morphology = cv2.copyMakeBorder(morphology,0,0,0,0,cv2.BORDER_CONSTANT,value=0)
 
 
+        if is_morphologyex is not None and is_morphologyex in ['dilate','erode','open','close']:
+            morphology = self.morphologyex_process(method = is_morphologyex,iterations = iterations)
+        
         # 这里传参要设置成int类型
         # https://docs.opencv.org/4.1.0/d3/dc0/group__imgproc__shape.html#gadf1ad6a0b82947fa1fe3c3d497f260e0
         # https://docs.opencv.org/4.1.0/d3/dc0/group__imgproc__shape.html#ga819779b9857cc2f8601e6526a3a5bc71
         if not isinstance(mode,int):
-            mode_dic = {'RETR_EXTERNAL':0, 'RETR_LIST':1, 'RETR_CCOMP':2, 'RETR_TREE':3, 'RETR_FLOODFILL':4}
+            mode_dic = {'RETR_EXTERNAL':cv2.RETR_EXTERNAL, 'RETR_LIST':cv2.RETR_LIST, 'RETR_CCOMP':cv2.RETR_CCOMP, 'RETR_TREE':cv2.RETR_TREE, 'RETR_FLOODFILL':cv2.RETR_FLOODFILL}
             mode = mode_dic[mode]
         # 本来指定使用版本是4.x，但考虑到很多人用的依旧是3.X,所以针对findContours的用法对两个版本都做了兼容处理.By Kwong 20190730
         if cv2.__version__[0]=='4':
-            cnts, _ = cv2.findContours(morphology.copy(),mode=mode,method=cv2.CHAIN_APPROX_SIMPLE)
+            cnts, hier = cv2.findContours(morphology,mode=mode,method=cv2.CHAIN_APPROX_SIMPLE)
         else:
-            _, cnts , _ = cv2.findContours(morphology.copy(),mode=mode,method=cv2.CHAIN_APPROX_SIMPLE)
-        return cnts    
+            _, cnts , hier = cv2.findContours(morphology,mode=mode,method=cv2.CHAIN_APPROX_SIMPLE)
+
+        if not is_hier_return:
+            return cnts
+        else:
+            return cnts , hier
 
     def draw_contours(self, cnts):
         """
@@ -145,7 +174,9 @@ class OpenCV:
             
     def resize(self,resize_shape_w,resize_shape_h,interpolation = 'default'):
         if interpolation == 'default':
-            if resize_shape_w < self._im.shape[1] and resize_shape_h < self._im.shape[0]:
+            if self._im.shape[1] < 100 and self._im.shape[0] < 100:
+                im_resize = cv2.resize(self._im,(int(resize_shape_w),int(resize_shape_h)), interpolation=cv2.INTER_NEAREST)
+            elif resize_shape_w < self._im.shape[1] and resize_shape_h < self._im.shape[0]:
                 im_resize = cv2.resize(self._im,(int(resize_shape_w),int(resize_shape_h)), interpolation=cv2.INTER_AREA)
             else:
                 im_resize = cv2.resize(self._im,(int(resize_shape_w),int(resize_shape_h)), interpolation=cv2.INTER_CUBIC)
